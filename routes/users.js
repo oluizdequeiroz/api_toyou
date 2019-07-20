bffd054739c6ebc758a3d3f0cc73cf3e20c7ec06
LuizDeQueiroz <luizdequeiroz@outlook.com.br>
2019-07-20 14:06:28 -0300

    criação do endpoint consultar histórico de saldo de usuário.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/index');
 
+const scriptSaldo = `SELECT id as saldoId, saldo FROM toyousoc_toyou_rede.wo_moedas WHERE user_id = :id GROUP BY saldo ORDER BY id desc`;
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   model.wo_users.findAll({})
@@ -17,12 +19,32 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/:id/saldo', (req, res, next) => {
-  model.sequelize.query(`SELECT max(id), saldo FROM wo_moedas WHERE user_id = ${req.params.id} GROUP BY saldo`,
-    { type: model.sequelize.QueryTypes.SELECT }
+  model.sequelize.query(scriptSaldo,
+    {
+      replacements: { id: req.params.id },
+      type: model.sequelize.QueryTypes.SELECT
+    }
   )
-    .then(user => res.json({
+    .then(result => res.json({
       error: false,
-      data: user[0].saldo
+      data: result && (result[0] && result[0].saldo)
+    }))
+    .catch(error => res.json({
+      error: true,
+      exception: error
+    }))
+});
+
+router.get('/:id/saldo/historico', (req, res, next) => {
+  model.sequelize.query(scriptSaldo,
+    {
+      replacements: { id: req.params.id },
+      type: model.sequelize.QueryTypes.SELECT
+    }
+  )
+    .then(result => res.json({
+      error: false,
+      data: result
     }))
     .catch(error => res.json({
       error: true,
