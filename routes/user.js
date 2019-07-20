685b75bcfbdabbdca54ebfcda7501c86958680ac
LuizDeQueiroz <luizdequeiroz@outlook.com.br>
2019-07-20 16:34:19 -0300

    separando endpoints de users (dados de user) de user (saldos).

---
+++ routes/user.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var router = express.Router();
+var model = require('../models/index');
+
+const scriptSaldo = `SELECT id as saldoId, saldo FROM toyousoc_toyou_rede.wo_moedas WHERE user_id = :id ORDER BY id desc`;
+const promiseScriptSaldo = id => model.sequelize.query(scriptSaldo,
+  {
+    replacements: { id },
+    type: model.sequelize.QueryTypes.SELECT
+  }
+);
+const promiseCreateSaldo = (user_id, saldo) => model.wo_moedas.create({ user_id, saldo });
+
+router.get('/:id/saldo', (req, res, next) => {
+  promiseScriptSaldo(req.params.id).then(result => res.json({
+    error: false,
+    data: result && (result[0] && result[0].saldo)
+  }))
+    .catch(error => res.json({
+      error: true,
+      exception: error
+    }));
+});
+
+router.get('/:id/saldo/historico', (req, res, next) => {
+  promiseScriptSaldo(req.params.id).then(result => res.json({
+    error: false,
+    data: result
+  }))
+    .catch(error => res.json({
+      error: true,
+      exception: error
+    }));
+});
+
+router.put('/:id/saldo/deducao', (req, res, next) => {
+  promiseScriptSaldo(req.params.id).then(result => {
+    const saldoAtual = (result && (result[0] && result[0].saldo)) || 0;
+    const novoSaldo = saldoAtual - req.body.saldo;
+
+    promiseCreateSaldo(req.params.id, novoSaldo)
+      .then(result => res.json({
+        error: false,
+        message: 'Valor deduzido no saldo!'
+      }))
+      .catch(error => res.json({
+        error: true,
+        exception: error
+      }));
+  })
+    .catch(error => res.json({
+      error: true,
+      exception: error
+    }));
+});
+
+router.put('/:id/saldo/adicao', (req, res, next) => {
+  promiseScriptSaldo(req.params.id).then(result => {
+    const saldoAtual = (result && (result[0] && result[0].saldo)) || 0;
+    const novoSaldo = saldoAtual + req.body.saldo;
+
+    promiseCreateSaldo(req.params.id, novoSaldo)
+      .then(result => res.json({
+        error: false,
+        message: 'Valor adicionado no saldo!'
+      }))
+      .catch(error => res.json({
+        error: true,
+        exception: error
+      }));
+  })
+    .catch(error => res.json({
+      error: true,
+      exception: error
+    }));
+});
+
+router.put('/:id/saldo/novo', (req, res, next) => {
+  promiseCreateSaldo(req.params.id, req.body.saldo).then(result => res.json({
+    error: false,
+    message: 'Novo saldo inserido com sucesso!'
+  }))
+    .catch(error => res.json({
+      error: true,
+      exception: error
+    }));
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/index');
 
-const scriptSaldo = `SELECT id as saldoId, saldo FROM toyousoc_toyou_rede.wo_moedas WHERE user_id = :id ORDER BY id desc`;
-const promiseScriptSaldo = id => model.sequelize.query(scriptSaldo,
-  {
-    replacements: { id },
-    type: model.sequelize.QueryTypes.SELECT
-  }
-);
-const promiseCreateSaldo = (user_id, saldo) => model.wo_moedas.create({ user_id, saldo });
+const simpleQuery = (sql, res) => model.sequelize.query(sql, { type: model.sequelize.QueryTypes.SELECT })
+  .then(result => res.json({
+    error: false,
+    data: result
+  }))
+  .catch(error => res.json({
+    error: true,
+    exception: error
+  }));
 
-/* GET users listing. */
 router.get('/', function (req, res, next) {
   model.wo_users.findAll({})
     .then(users => res.json({
@@ -25,79 +25,19 @@ router.get('/', function (req, res, next) {
     }));
 });
 
-router.get('/:id/saldo', (req, res, next) => {
-  promiseScriptSaldo(req.params.id).then(result => res.json({
-    error: false,
-    data: result && (result[0] && result[0].saldo)
-  }))
-    .catch(error => res.json({
-      error: true,
-      exception: error
-    }));
-});
+router.get('/:column/:type/:value', (req, res, next) => {
+  const { column, type } = req.params;
 
-router.get('/:id/saldo/historico', (req, res, next) => {
-  promiseScriptSaldo(req.params.id).then(result => res.json({
-    error: false,
-    data: result
-  }))
-    .catch(error => res.json({
-      error: true,
-      exception: error
-    }));
-});
-
-router.put('/:id/saldo/deducao', (req, res, next) => {
-  promiseScriptSaldo(req.params.id).then(result => {
-    const saldoAtual = (result && (result[0] && result[0].saldo)) || 0;
-    const novoSaldo = saldoAtual - req.body.saldo;
-
-    promiseCreateSaldo(req.params.id, novoSaldo)
-      .then(result => res.json({
-        error: false,
-        message: 'Valor deduzido no saldo!'
-      }))
-      .catch(error => res.json({
-        error: true,
-        exception: error
-      }));
-  })
-    .catch(error => res.json({
-      error: true,
-      exception: error
-    }));
-});
-
-router.put('/:id/saldo/adicao', (req, res, next) => {
-  promiseScriptSaldo(req.params.id).then(result => {
-    const saldoAtual = (result && (result[0] && result[0].saldo)) || 0;
-    const novoSaldo = saldoAtual + req.body.saldo;
-
-    promiseCreateSaldo(req.params.id, novoSaldo)
-      .then(result => res.json({
-        error: false,
-        message: 'Valor adicionado no saldo!'
-      }))
-      .catch(error => res.json({
-        error: true,
-        exception: error
-      }));
-  })
-    .catch(error => res.json({
-      error: true,
-      exception: error
-    }));
-});
-
-router.put('/:id/saldo/novo', (req, res, next) => {
-  promiseCreateSaldo(user_id, saldo).then(result => res.json({
-    error: false,
-    message: 'Novo saldo inserido com sucesso!'
-  }))
-    .catch(error => res.json({
-      error: true,
-      exception: error
-    }));
+  if (type === 'like') {
+    simpleQuery(`SELECT * FROM wo_users WHERE ${column} like '%${req.params.value}%'`, res);
+  } else {
+    const value = type === 'int' ? parseInt(req.params.value) : (type === 'float' ? parseFloat(req.params.value) : `'${req.params.value}'`);
+    if (type === 'in') {
+      simpleQuery(`SELECT * FROM wo_users WHERE ${column} in (${value.split(",").join("','")})`, res);
+    } else {
+      simpleQuery(`SELECT * FROM wo_users WHERE ${column} = ${value}`, res);
+    }
+  }
 });
 
 module.exports = router;
